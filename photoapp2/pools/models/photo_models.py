6b8efd85d78afe69871800739b08789d3ad56913
Alex <aleksandreremeev75@gmail.com>
2023-10-13 08:44:14 +0300

    update

--- photoapp2/pools/models/photo_models.py
+++ photoapp2/pools/models/photo_models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Photo(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to='photos/')
+    image = models.CharField(max_length=500)
     upload_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, choices=[('moderation', 'На модерации'), ('approved', 'Одобрена'), ('deleted', 'На удалении')])
     author = models.ForeignKey(User, on_delete=models.CASCADE)
