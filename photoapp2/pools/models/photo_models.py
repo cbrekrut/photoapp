36b856a2be4013cbadb06f014ecaf928b058dc59
Alex <aleksandreremeev75@gmail.com>
2023-10-14 23:09:33 +0300

    add static

--- photoapp2/pools/models/photo_models.py
+++ photoapp2/pools/models/photo_models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Photo(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.CharField(max_length=500)
+    image = models.CharField()
     upload_date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, choices=[('moderation', 'На модерации'), ('approved', 'Одобрена'), ('deleted', 'На удалении')])
+    status = models.CharField(max_length=20, choices=[('moderation', 'На модерации'), ('approved', 'Одобрена'), ('deleted', 'На удалении')],default='approved')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
